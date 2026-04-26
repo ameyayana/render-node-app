9dd2d4b5933d62cc4513fbc83c6c2393207c388e
ameyayana <amelia6675@gmail.com>
2026-04-26 16:56:45 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.MESSAGE || "Hello from Render Node App 🚀";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`<h1>${MESSAGE}</h1>`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
